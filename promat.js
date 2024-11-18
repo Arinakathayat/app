f62ff2be0c7129e6e25b304459a3b5b3ee4151a2
Alina Kathayat <ariasushi01@gmail.com>
2024-11-18 08:37:23 +0545

    Update promat.js

--- promat.js
+++ promat.js
@@ -1,9 +1,11 @@
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}
+                                                    );
 
 readline.question('What is your name? ', name => {
     greet(name);
     readline.close();
-});
+}
+                 );
